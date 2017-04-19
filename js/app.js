e27f155344f7c96618cb76deb1c340febe1e8268
Krishna <krishna.kamani@gmail.com>
2017-04-18 22:55:23 -0700

    Added Sorting

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-        $(document).ready(function () {
-            //Sample Json data
-            var data = [
+var jsondata = [
                     { 
                         "id": "1", 
                         "title":"Gone Girl",
@@ -44,40 +42,25 @@
                         "releaseDate": "2015-01-01"
                     }];
 
-
-            var jsondata = data;
-
-            console.log(jsondata);
-
-            var c = '';
-            $(function() {
-                $.each(jsondata, function(i, item) {
-                     c += '<div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><img src="'+item.image+'" alt=""><div class="caption"><h4>'+item.title+'</h4></div><div class="caption"><h5>'+item.releaseDate+'</h5></div><div class="ratings"><input type="text" id="rating-input" class="rating rating-loading" value="'+item.rating+'" data-size="xs" title=""';
-                        
-                        var arrDate = item.releaseDate.split("-");
-                        var today = new Date();
-                        useDate = new Date(arrDate[0], arrDate[1] - 1, arrDate[2]);
-
-                        console.log(useDate);
-                          console.log(today);
-                              console.log("------------");
-
-                        if (useDate > today) {
-                             c = c+ 'disabled';
-                        } 
-
-                        c +='></div></div></div>';
-                });
-                    $('#dynamicData').empty().html(c).fadeIn();
-                    
- // Star Rating is not displaying when addding dynamically hence refreshing .
-        $("input[id*='rating-input']").each(function (i, el) {
-               $(el).rating('refresh');
-        });
-                });
-
-
-        refreshGraph(jsondata);
+        $(document).ready(function () {
+            $("#pick").change(function(){
+                 var status = this.value;
+                 if(status == 'Titleascending'){
+                    jsondata.sort(dynamicSort("title",1));
+                    drawPage();
+                 } else if(status == 'Titledecending'){
+                    jsondata.sort(dynamicSort("title",-1));
+                    drawPage();
+                 } else if(status == 'Ratingascending'){
+                    jsondata.sort(dynamicSort("rating",1));
+                    drawPage();
+                 } else if(status == 'Ratingdecending'){
+                    jsondata.sort(dynamicSort("rating",-1));
+                    drawPage();
+                 }
+              });
+            
+            drawPage();
 
         });
 
@@ -131,3 +114,61 @@ function refreshGraph(jsondata){
             }
         });
     }
+
+
+    function drawPage() {
+                var c = '';
+                $.each(jsondata, function(i, item) {
+                     c += '<div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><img src="'+item.image+'" alt=""><div class="caption"><h4>'+item.title+'</h4></div><div class="caption"><h5>'+item.releaseDate+'</h5></div><div class="ratings"><input type="text" id="rating-input" name="'+item.id+'" class="rating rating-loading" value="'+item.rating+'" data-size="xs" title=""';
+                        
+                        var arrDate = item.releaseDate.split("-");
+                        var today = new Date();
+                        useDate = new Date(arrDate[0], arrDate[1] - 1, arrDate[2]);
+
+                        if (useDate > today) {
+                             c = c+ 'disabled';
+                        } 
+
+                        c +='></div></div></div>';
+                });
+                    $('#dynamicData').empty().html(c).fadeIn();
+                    
+            // Star Rating is not displaying when addding dynamically hence refreshing .
+                $("input[id*='rating-input']").each(function (i, el) {
+                    $(el).rating('refresh');
+                    $(el).bind("rating.change", function(){
+                        var idToBeChanged =  $(this).attr("name");  
+                        var newRating = $(this).val();
+                                console.log(idToBeChanged);
+                            $.each(jsondata, function(i, item) {
+                                console.log(item.id);
+                                 if(item.id == idToBeChanged){
+                                      console.log(item.rating);
+                                        item.rating = newRating;
+                                         console.log(item.rating);
+                                         return false;
+                                 }
+                            });
+
+
+                            refreshGraph(jsondata);
+
+
+
+                    }); // Bind the event as Jquery do not consider added loaded elements
+                });
+                        refreshGraph(jsondata);
+            }
+
+
+            function dynamicSort(property , sortOrder) {
+              //  var sortOrder = 1;
+                if(property[0] === "-") {
+                    sortOrder = -1;
+                    property = property.substr(1);
+                }
+                return function (a,b) {
+                    var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+                    return result * sortOrder;
+                }
+            }
